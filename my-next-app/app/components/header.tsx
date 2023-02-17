0fb14cc87e33f3c9424a7bda4a850d1cdd1c9f56
Ebibeck <daziebiatri909@gmail.com>
2023-02-17 15:27:35 +0100

    commit

---
+++ my-next-app/app/components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className=" flex w-full h-16 items-center justify-between px-4 mt-9 mx-2 rounded-lg">
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
--- my-next-app/app/page.tsx
+++ my-next-app/app/page.tsx
@@ -1,3 +1,11 @@
- export default function Home(){
-  return
- }
+import Header from "./components/header";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="h-screen w-2/5 flex bg-slate-600 rounded-lg">
+        <Header/>
+      </div>
+    </div>
+  );
+}
